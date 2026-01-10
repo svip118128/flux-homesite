498ab083e1cdc850b1fc5ffc694dc4e893521eaf
Nguyen Ngoc Khanh <nguyenngockhanhbl@gmail.com>
2026-01-10 18:22:08 +0700

    update link downloads

--- app/config/downloads.ts
+++ app/config/downloads.ts
@@ -14,31 +14,31 @@ export interface DownloadConfig {
 export const downloadUrls: Record<string, Record<string, DownloadConfig>> = {
     windows: {
         win64: {
-            url: 'https://antitheft-server.onrender.com/api/products/antitheft/downloads/1.0/setup.exe',
-            version: '0.0.6',
-            filename: 'jdchat-amd64-dev-gw-0.0.6-20260102.rar'
+            url: 'https://og.jdchat.im/jdchatapp/pc/jdchat-amd64-20260110-06184ee9.exe.zip',
+            version: '0.0.8',
+            filename: 'jdchat-amd64-20260110-06184ee9.exe.zip'
         },
         win32: {
-            url: 'https://antitheft-server.onrender.com/api/products/antitheft/downloads/1.0/setup.exe',
-            version: '0.0.6',
-            filename: 'jdchat-x86-dev-gw-0.0.6-20260102.rar'
+            url: 'https://og.jdchat.im/jdchatapp/pc/jdchat-amd64-20260110-06184ee9.exe.zip',
+            version: '0.0.8',
+            filename: 'jdchat-amd64-20260110-06184ee9.exe.zip'
         },
         arm64: {
-            url: 'https://antitheft-server.onrender.com/api/products/antitheft/downloads/1.0/setup.exe',
-            version: '0.0.6',
-            filename: 'jdchat-arm64-dev-gw-0.0.6-20260102.rar'
+            url: 'https://og.jdchat.im/jdchatapp/pc/jdchat-amd64-20260110-06184ee9.exe.zip',
+            version: '0.0.8',
+            filename: 'jdchat-amd64-20260110-06184ee9.exe.zip'
         }
     },
     mac: {
         silicon: {
-            url: 'https://antitheft-server.onrender.com/api/products/antitheft/downloads/1.0/setup.exe',
-            version: '0.0.6',
-            filename: 'jdchat-arm64-dev-gw-0.0.6-20260102.zip'
+            url: 'https://og.jdchat.im/jdchatapp/mac/dchat-arm64-20260110-7eee9b74.dmg',
+            version: '0.0.8',
+            filename: 'dchat-arm64-20260110-7eee9b74.dmg'
         },
         intel: {
-            url: 'https://antitheft-server.onrender.com/api/products/antitheft/downloads/1.0/setup.exe',
-            version: '0.0.6',
-            filename: 'jdchat-amd64-dev-gw-0.0.6-20260102.zip'
+            url: 'https://og.jdchat.im/jdchatapp/mac/jdchat-amd64-2026010-e20d9439.dmg',
+            version: '0.0.8',
+            filename: 'jdchat-amd64-2026010-e20d9439.dmg'
         }
     }
 }
