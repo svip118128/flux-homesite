3961032478772bf6a98eae5d9e8944a47d0e56b9
Nguyen Ngoc Khanh <nguyenngockhanhbl@gmail.com>
2026-02-03 08:59:09 +0700

    feat: update release build application

--- app/config/index.ts
+++ app/config/index.ts
@@ -11,44 +11,44 @@ export interface DownloadConfig {
 export const downloadUrls: Record<string, Record<string, DownloadConfig>> = {
     windows: {
         win64: {
-            url: 'https://og.jdchat.im/jdchatapp/pc/jdchat-amd64-20260110-06184ee9.exe.zip',
-            version: '0.0.8',
-            filename: 'jdchat-amd64-20260110-06184ee9.exe.zip'
+            url: 'https://og.jdchat.im/jdchatapp/pc/jdchat-amd64-20260130-1.0.3-9ef932c0.exe.zip',
+            version: '1.0.3',
+            filename: 'jdchat-amd64-20260130-1.0.3-9ef932c0.exe.zip'
         },
         win32: {
-            url: 'https://og.jdchat.im/jdchatapp/pc/jdchat-amd64-20260110-06184ee9.exe.zip',
-            version: '0.0.8',
-            filename: 'jdchat-amd64-20260110-06184ee9.exe.zip'
+            url: 'https://og.jdchat.im/jdchatapp/pc/jdchat-amd64-20260130-1.0.3-9ef932c0.exe.zip',
+            version: '1.0.3',
+            filename: 'jdchat-amd64-20260130-1.0.3-9ef932c0.exe.zip'
         },
         arm64: {
-            url: 'https://og.jdchat.im/jdchatapp/pc/jdchat-amd64-20260110-06184ee9.exe.zip',
-            version: '0.0.8',
-            filename: 'jdchat-amd64-20260110-06184ee9.exe.zip'
+            url: 'https://og.jdchat.im/jdchatapp/pc/jdchat-amd64-20260130-1.0.3-9ef932c0.exe.zip',
+            version: '1.0.3',
+            filename: 'jdchat-amd64-20260130-1.0.3-9ef932c0.exe.zip'
         }
     },
     mac: {
         silicon: {
-            url: 'https://og.jdchat.im/jdchatapp/mac/dchat-arm64-20260110-7eee9b74.dmg',
-            version: '0.0.8',
-            filename: 'dchat-arm64-20260110-7eee9b74.dmg'
+            url: 'https://og.jdchat.im/jdchatapp/mac/jdchat-arm64-20260130-1.0.3-76c2929b.dmg',
+            version: '1.0.3',
+            filename: 'jdchat-arm64-20260130-1.0.3-76c2929b.dmg'
         },
         intel: {
-            url: 'https://og.jdchat.im/jdchatapp/mac/jdchat-amd64-2026010-e20d9439.dmg',
-            version: '0.0.8',
-            filename: 'jdchat-amd64-2026010-e20d9439.dmg'
+            url: 'https://og.jdchat.im/jdchatapp/mac/jdchat-amd64-20260130-1.0.3-695b20a9.dmg',
+            version: '1.0.3',
+            filename: 'jdchat-amd64-20260130-1.0.3-695b20a9.dmg'
         }
     }
 }
 
 export const androidDownloadUrl: DownloadConfig = {
     url: 'https://play.google.com/store/apps/details?id=app.jdchat.me',
-    version: '1.0.0',
-    filename: 'jdchat-release-gw-1.0.0-20251226.apk'
+    version: '1.0.3',
+    // filename: 'jdchat-release-gw-1.0.0-20251226.apk'
 }
 
 export const iosDownloadUrl: DownloadConfig = {
     url: 'https://apps.apple.com/us/app/jdchat/id6756490888',
-    version: '1.0.0'
+    version: '1.0.3'
 }
 
 export const customScheme = 'www.jdchat.im'
